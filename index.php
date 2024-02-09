d1e63e44b26f61a0d93e1691781b97d314de82f3
nishantdas29 <157672766+nishantdas29@users.noreply.github.com>
2024-02-09 14:50:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,97 +1,127 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Facebook Profile</title>
-    <link rel="stylesheet" type="text/css" href="facebook profile.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <style>
+    body {
+      font-size: 16px;
+      margin: 0;
+      font-family: 'Roboto', sans-serif;
+      box-sizing: border-box;
+      background-color: #f0f0f0;
+      color: #333;
+    }
+    header {
+      background-color: #4CAF50;
+      color: #fff;
+      padding: 10px;
+      text-align: center;
+    }
+    h1, h2, h3, h4 {
+      text-transform: uppercase;
+      margin: 0;
+      color: #333;
+    }
+    hr {
+      border: 1px solid #777;
+    }
+    img {
+      border-radius: 50%;
+      border: 3px solid #4CAF50;
+    }
+    table {
+      border-collapse: collapse;
+      width: 70%;
+      margin-top: 20px;
+    }
+    th, td {
+      border: 2px solid #555;
+      padding: 10px;
+      text-align: center;
+    }
+    th {
+      background-color: #4CAF50;
+      color: #fff;
+    }
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+    li {
+      margin-bottom: 8px;
+    }
+  </style>
 </head>
 <body>
-    <div class="column1">
-        <nav class="navbar">
-            <div class="logo">
-                <img src="Facebook_Logo_2023.png" alt="logo">
-            </div>
+  <header>
+    <h1>My Portfolio</h1>
+  </header>
+  <div style="display: flex; justify-content: center; margin-bottom: 20px;">
+    <table>
+      <tr>
+        <td><img src="img/IMG_20240121_172831.jpg" alt="Profile Image" width="200" height="200"></td>
+        <td>
+          <ul>
+            <li><b>NAME</b> - Madhur</li>
+            <li><b>UID</b> - 23MCC20057</li>
+  
+           
+            <li><b>ADDRESS</b> - Unna, Himanchal Pradesh</li>
+          </ul>
+        </td>
+      </tr>
+    </table>
+  </div>
+  <header>
+    <h2>Qualification Details</h2>
+  </header>
+  <div style="display: flex; justify-content: center; margin-bottom: 20px;">
+    <table border="1" width="70%">
+      <tr>
+        <th>Qualification</th>
+        <th>Stream</th>
+        <th>Col/Scl/Instt</th>
+        <th>Pass Year</th>
+        <th>Percentage</th>
+      </tr>
+      <tr>
+        <td>10th</td>
+        <td>General</td>
+        <td>DAV Public School</td>
+        <td>2018</td>
+        <td>75.2</td>
+      </tr>
+      <tr>
+        <td>12th</td>
+        <td>Science</td>
+        <td>DAV Public School</td>
+        <td>2020</td>
+        <td>68.45</td>
+      </tr>
+      <tr>
+        <td>Graduation</td>
+        <td>BCA</td>
+        <td>Himanchal University</td>
+        <td>2023</td>
+        <td>80.5</td>
+      </tr>
+    </table>
+  </div>
+  <header>
+    <h2>Strength</h2>
+  </header>
+  <ul style="padding-left: 20px">
+    <li>Loyality</li>
+    <li>Honest</li>
+    <li>Confident</li>
+  </ul>
 
-            <div class="search-facebook">
-                <i class="fa-solid fa-magnifying-glass"><input type="text" placeholder="Search Facebook"></i>
-                
-            </div>
-
-            <div class="nav-center">
-                <a href=""><i class="fa-solid fa-house"></i></a>
-                <a href=""><i class="fa-brands fa-youtube"></i></a>
-                <a href=""><i class="fa-solid fa-store"></i></a>
-                <a href=""><i class="fa-solid fa-people-group"></i></a>
-                <a href=""><i class="fa-solid fa-gamepad"></i></a>
-            </div>
-
-            <div class="nav-right">
-                <a href=""><i class="fa-solid fa-bars"></i></a>
-                <a href=""><i class="fa-solid fa-plus"></i></a>
-                <a href=""><i class="fa-brands fa-facebook-messenger"></i></a>
-                <a href=""><i class="fa-solid fa-bell"></i></a>
-                <img src="WhatsApp Image 2024-02-04 at 14.45.17_dc578736.jpg" alt="img">
-            </div>
-        </nav>
-
-        <div class="container">
-            <div class="coverphoto">
-                <img src="WhatsApp Image 2024-02-04 at 14.03.32_bb5da717.jpg" alt="img">
-                <button type="button"><a href=""><i class="fa-solid fa-camera"></i>Edit cover photo</a></button>
-            </div>
-
-            <div class="profile-picture">
-                <img src="WhatsApp Image 2024-02-04 at 14.45.17_dc578736.jpg" alt="img">
-                <a href=""><i class="fa-solid fa-camera"></i></a>
-            </div>
-
-            <div class="short-details">
-                <h1>Nishant Kumar</h1>
-                <p>1.1k friends</p>
-
-                <div class="profile-button">
-                    <div class="add-story">
-                        <button type="button">+ Add to story</button>
-                    </div>
-    
-                    <div class="edit-profile">
-                        <button type="button">Edit profile</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="content-link">
-                <a href="">Posts</a>
-                <a href="">About</a>
-                <a href="">Friends</a>
-                <a href="">Photos</a>
-                <a href="">Videos</a>
-                <a href="">Reels</a>
-                <a href="">More<i class="fa-solid fa-caret-down"></i></a>
-            </div>
-        </div>
-    </div>
-
-    <div class="column2">
-        <div class="profile-details">
-            <div class="intro">
-                <h1>Intro</h1>
-            </div>
-
-            <div class="bio">
-                <p></p>
-                <button>Edit Bio</button>
-            </div>
-
-            <div class=""></div>
-
-        </div>
-
-        <div class="de"></div>
-    </div>
-
-    <div class="column3">
-        <div class="profile-content"></div> 
-    </div>
+  <header>
+    <h2>Interests</h2>
+  </header>
+  <ul style="padding-left: 20px">
+    <li>Walking</li>
+    <li>Listening to Audiobook</li>
+    <li>Gaming</li>
+  </ul>
 </body>
 </html>

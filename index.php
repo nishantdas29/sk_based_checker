b3a090e752ae3e5e524218f5ef93e7f27e810630
nishantdas29 <157672766+nishantdas29@users.noreply.github.com>
2024-02-09 14:44:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,26 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="light.css">
 </head>
 <body>
-    <u><h1>Registration Form</h1></u>
-    <form action="">
-        First Name: <br><input><br>
-        Last Name: <br><input><br>
-        Gender: <br>
-        <input type="radio" id="Male" name="gender" value="male">
-        <table for="Male">Male</table><br>
-        <input type="radio" id="Female" name="gender" value="Female">
-        <table for="Female">Female</table><br>
-        Mobile Number: <br><input><br>
-        Email Id: <br><input><br>
-       <p> Complete Address: <br><textarea name="address" id="address" cols="20" rows="5"></textarea></p>
-       <p>Are you 18+ ?</p>
-       Yes<input type="radio" name="Age" value="Yes"><br>
-       No<input type="radio" name="Age" value="No"><br>
-        <input type="submit" value="Sign Up">
+    <h1 contenteditable="true">hello</h1><br>
+    <div class="animated-text">
+        I'm a <span></span>
+    </div>
         
-        
-    </form>
 </body>
 </html>

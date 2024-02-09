c71295a02abfc925cade5ed22737547a4973672e
nishantdas29 <157672766+nishantdas29@users.noreply.github.com>
2024-02-09 14:47:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="light.css">
+    <title>Facebook Profile</title>
+    <link rel="stylesheet" type="text/css" href="facebook profile.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <h1 contenteditable="true">hello</h1><br>
-    <div class="animated-text">
-        I'm a <span></span>
+    <div class="column1">
+        <nav class="navbar">
+            <div class="logo">
+                <img src="Facebook_Logo_2023.png" alt="logo">
+            </div>
+
+            <div class="search-facebook">
+                <i class="fa-solid fa-magnifying-glass"><input type="text" placeholder="Search Facebook"></i>
+                
+            </div>
+
+            <div class="nav-center">
+                <a href=""><i class="fa-solid fa-house"></i></a>
+                <a href=""><i class="fa-brands fa-youtube"></i></a>
+                <a href=""><i class="fa-solid fa-store"></i></a>
+                <a href=""><i class="fa-solid fa-people-group"></i></a>
+                <a href=""><i class="fa-solid fa-gamepad"></i></a>
+            </div>
+
+            <div class="nav-right">
+                <a href=""><i class="fa-solid fa-bars"></i></a>
+                <a href=""><i class="fa-solid fa-plus"></i></a>
+                <a href=""><i class="fa-brands fa-facebook-messenger"></i></a>
+                <a href=""><i class="fa-solid fa-bell"></i></a>
+                <img src="WhatsApp Image 2024-02-04 at 14.45.17_dc578736.jpg" alt="img">
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="coverphoto">
+                <img src="WhatsApp Image 2024-02-04 at 14.03.32_bb5da717.jpg" alt="img">
+                <button type="button"><a href=""><i class="fa-solid fa-camera"></i>Edit cover photo</a></button>
+            </div>
+
+            <div class="profile-picture">
+                <img src="WhatsApp Image 2024-02-04 at 14.45.17_dc578736.jpg" alt="img">
+                <a href=""><i class="fa-solid fa-camera"></i></a>
+            </div>
+
+            <div class="short-details">
+                <h1>Nishant Kumar</h1>
+                <p>1.1k friends</p>
+
+                <div class="profile-button">
+                    <div class="add-story">
+                        <button type="button">+ Add to story</button>
+                    </div>
+    
+                    <div class="edit-profile">
+                        <button type="button">Edit profile</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="content-link">
+                <a href="">Posts</a>
+                <a href="">About</a>
+                <a href="">Friends</a>
+                <a href="">Photos</a>
+                <a href="">Videos</a>
+                <a href="">Reels</a>
+                <a href="">More<i class="fa-solid fa-caret-down"></i></a>
+            </div>
+        </div>
     </div>
-        
+
+    <div class="column2">
+        <div class="profile-details">
+            <div class="intro">
+                <h1>Intro</h1>
+            </div>
+
+            <div class="bio">
+                <p></p>
+                <button>Edit Bio</button>
+            </div>
+
+            <div class=""></div>
+
+        </div>
+
+        <div class="de"></div>
+    </div>
+
+    <div class="column3">
+        <div class="profile-content"></div> 
+    </div>
 </body>
 </html>
